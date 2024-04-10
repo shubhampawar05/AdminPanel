0eb6ec83ec67ab8a458e743348713198d16f1b27
Shubham Chopde <shubhamchopde090@gmail.com>
2024-04-10 16:49:32 +0530

    adding some new things

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -1,21 +1,22 @@
-import "./style.css"
-import Sidebar from './../../components/sidebar/Sidebar'
-import Header from './../../components/header/Header'
-import DataTable from "../../components/dataTable/DataTable"
-
-
+import "./style.css";
+import Sidebar from "./../../components/sidebar/Sidebar";
+import Header from "./../../components/header/Header";
+import DataTable from "../../components/dataTable/DataTable";
+import ContainerWrapper from './../../components/containerWrapper/ContainerWrapper'
 const List = () => {
   return (
     <div className="list">
-      <Sidebar/>
-      <div className="listContainerListPage">
-        <Header/>
-        <div className="dataTable">
-        <DataTable/>
-        </div>
-      </div>
+      <Sidebar />
+      <Header />
+      <ContainerWrapper>
+        
+          <div className="dataTable">
+            <DataTable />
+          </div>
+        
+      </ContainerWrapper>
     </div>
-  )
-}
+  );
+};
 
-export default List
+export default List;
